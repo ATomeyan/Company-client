72335b139ab428e44e5320b6f6b3e57f5821595b
arturto <tomeyan.artur@gmail.com>
2022-05-12 12:06:27 +0400

    Initial

---
+++ src/app/components/dialogs/count-dialog/count-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-count-dialog',
+  templateUrl: './count-dialog.component.html',
+  styleUrls: ['./count-dialog.component.css']
+})
+export class CountDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/dialogs/count-dialog/count.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CountDialogComponent} from "./count-dialog.component";
+
+@NgModule({
+  declarations: [CountDialogComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountModule { }
